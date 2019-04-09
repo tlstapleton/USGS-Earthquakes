0f611ea90992f8bd6760cd470d76dc85e2e337a1
Toni Stapleton <tonistpltn25@gmail.com>
2019-04-09 13:38:53 -0500

    add baselayers

--- logic.js
+++ logic.js
@@ -1,19 +1,6 @@
 
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
 
-var myMap = L.map("map", {
-    center: [10, 0],
-    zoom: 2.4
-  });
-  
-
-var darkmap=L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-maxZoom: 18,
-id: "mapbox.dark",
-accessToken: API_KEY
-}).addTo(myMap);
-
 
 d3.json(queryUrl, function(data) {
   
@@ -21,31 +8,6 @@ d3.json(queryUrl, function(data) {
   createFeatures(data.features);
 
 });
-
-
-
-var legend = L.control({position: 'bottomright'});
-
-legend.onAdd = function (myMap) {
-
-    var div = L.DomUtil.create('div', 'info legend'),
-        grades = [0, 1,2,3,4,5],
-        labels = [];
-
-    // loop through our density intervals and generate a label with a colored square for each interval
-    for (var i = 0; i < grades.length; i++) {
-        div.innerHTML +=
-            '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
-            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
-    }
-
-    return div;
-};
-
-legend.addTo(myMap);
-
-
-
 
 
 function getColor(d) {
@@ -59,17 +21,6 @@ function getColor(d) {
 
 function createFeatures(earthquakeData) {
 
-    // console.log(earthquakeData.features.properties);
-
-    // var geojsonMarkerOptions = {
-    //     radius: feature.properties.mag,
-    //     fillColor: "#ff7800",
-    //     color: "#000",
-    //     weight: 1,
-    //     opacity: 1,
-    //     fillOpacity: 0.8
-    // };
-
   var earthquakes = L.geoJSON(earthquakeData,{
     pointToLayer: function (feature, latlng) {
         return L.circleMarker(latlng, {
@@ -80,8 +31,77 @@ function createFeatures(earthquakeData) {
             opacity: 1,
             fillOpacity: 0.8
         });
-    }}).addTo(myMap);
+    }})
 
+    createMap(earthquakes);
  
 }
+
+
+function createMap(earthquakes) {
+  
+    var darkmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+      attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+      maxZoom: 18,
+      id: "mapbox.dark",
+      accessToken: API_KEY
+    });
+
+    var satellitemap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        id: "mapbox.satellite",
+        accessToken: API_KEY
+      });
+
+    var lightmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.light",
+    accessToken: API_KEY
+    });
+
+    var baseMaps = {
+    "Satellite": satellitemap,
+    "Dark Map": darkmap,
+    "Light Map":lightmap
+    };
+    
+    var overlayMaps = {
+      Earthquakes: earthquakes
+    };
+  
+   
+    var myMap = L.map("map", {
+        center: [10, 0],
+        zoom: 2.4,
+        layers: [darkmap, earthquakes]
+        });
+  
+   
+    L.control.layers(baseMaps,overlayMaps, {
+      collapsed: false
+    }).addTo(myMap);
+
+    var legend = L.control({position: 'bottomright'});
+
+    legend.onAdd = function (myMap) {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+        grades = [0, 1,2,3,4,5],
+        labels = [];
+
+    
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    }
+
+    return div;
+    };
+
+    legend.addTo(myMap);
+  }
+  
 
